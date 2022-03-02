8daa4a61437655c447b95d482f9ba120f966cf81
yoav <yoavgecht@gmail.com>
2022-03-02 11:45:53 +0200

    Changed port ref from hard coded to port var

--- src/api/TodosApi.js
+++ src/api/TodosApi.js
@@ -1,14 +1,15 @@
+const port = process.env.PORT || 6565;
 import axios from "axios";
 
 export default {
   async list() {
-    const response = await axios.get("http://localhost:6565/todos");
+    const response = await axios.get(`http://localhost:${port}/todos`);
     return response.data;
   },
   async add(todo) {
-    await axios.post(`http://localhost:6565/todos`, todo);
+    await axios.post(`http://localhost:${port}/todos`, todo);
   },
   async remove(id) {
-    await axios.delete(`http://localhost:6565/todos/${id}`);
+    await axios.delete(`http://localhost:${port}/todos/${id}`);
   },
 };
