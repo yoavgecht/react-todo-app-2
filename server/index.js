823d0ce494622755b25fb77cd51c536946a4a42a
yoav <yoavgecht@gmail.com>
2022-03-04 10:53:38 +0200

    dubug

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 6565;
-
+console.log("PORT", port);
 
 app.use(cors());
 app.use(express.json());
